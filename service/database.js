853356f1df75ad02a8b7fc4bccf04733e95daee1
Tony P <aphasay@outlook.com>
2025-11-13 21:58:13 -0700

    Connected mongo test

--- service/database.js
+++ service/database.js
@@ -4,5 +4,40 @@ const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
-const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const recipes = db.recipes('cookie');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+try {
+    const cookie = {
+        name: "Cookie",
+        summary: "testing a sugar cookie",
+    };
+    await recipes.insertOne(cookie);
+} catch (ex) {
+    console.log("Hello");
+}
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
